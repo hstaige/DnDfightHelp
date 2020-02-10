51320008c0900da5692acd94da55db83061b8df0
staigerh1 <staigerh1@gmail.com>
2020-02-10 18:35:58 -0500

    Reformatted File to match Pycharm recommendations

--- DnD.py
+++ DnD.py
@@ -1,24 +1,26 @@
 import random
 import math
 
+
 def rollD20():
-    return(random.randint(1,20))
+    return (random.randint(1, 20))
+
 
 def sortByIntiative(arr):
     num = len(arr)
     for i in range(num):
-        for j in range(0, num-i-1):
-            if arr[j].initiative > arr[j+1].initiative :
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+        for j in range(0, num - i - 1):
+            if arr[j].initiative > arr[j + 1].initiative:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
+    for i in range(math.floor(num / 2)):
+        arr[i], arr[num - i - 1] = arr[num - i - 1], arr[i]
 
-    for i in range(math.floor(num/2)):
-        arr[i],arr[num-i-1] = arr[num-i-1],arr[i]
+    return (arr)
 
-    return(arr)
 
 class combatant():
-    def __init__(self, name,enemOrFriend, dexMod):
+    def __init__(self, name, enemOrFriend, dexMod):
         self.name = name
         self.enemOrFriend = enemOrFriend
         self.initiative = rollD20() + dexMod
@@ -28,17 +30,16 @@ def main():
     combats = []
 
     numCombats = input('How many combatants are there? ')
-    for idx in range(int(numCombats)): #creates all combatants
-        idx = str(idx+1)
+    for idx in range(int(numCombats)):  # creates all combatants
+        idx = str(idx + 1)
         name = 'combatant' + idx
-        name = combatant(name,1,2)
+        name = combatant(name, 1, 2)
         combats.append(name)
 
     combats = sortByIntiative(combats)
 
-
-
     for fighter in combats:
         print('%-2d %s' % (fighter.initiative, fighter.name))
+
 
 main()
