71fac3f39b8a83847e8df47d487f4f4a235ecf42
staigerh1 <staigerh1@gmail.com>
2020-02-12 09:55:29 -0500

    Added damage tracking and death.:

--- DnD.py
+++ DnD.py
@@ -19,10 +19,31 @@ def sortByIntiative(arr):
     return arr
 
 
+def createNPCfighters(combats):
+    numCombats = int(input('How many enemys are there? '))
+    for idx in range(numCombats):  # creates all npc combatants
+        idx = str(idx + 1)
+        name = 'Enemy' + ' ' + idx
+        name = combatant(name, 1, 2)
+        combats.append(name)
+
+    return combats
+
+
+def addPCs(combats):
+    numPlayers = int(input('How many PCs are there? '))
+    for idx in range(numPlayers):
+        playerName = input('What is the name of PC %d? ' % (idx+1))
+        PC = combatant(playerName, 0, 2)
+        combats.append(PC)
+
+    return combats
+
+
 def printFighters(roster):
     for fighter in roster:
         print('%-2d %s' % (fighter.initiative, fighter.name))
-    print('All Done!\n\n')
+    print('All Done!\n')
 
 
 def goDownList(roster):
@@ -35,14 +56,21 @@ def mainGameLoop(roster):
     notExiting = True
     timer = 0
 
+    printFighters(roster)
+
     while notExiting:
-        if keyboard.is_pressed('n') and timer > 10000:
+        if keyboard.is_pressed('n') and timer > 10000: #Ends Turn
             roster = goDownList(roster)
             printFighters(roster)
             timer = 0
-        if keyboard.is_pressed('e'):
+        if keyboard.is_pressed('e'): #Exits program
             notExiting = False
-        timer += 1
+        if keyboard.is_pressed('a') and timer>10000: #Makes attack
+            attacker = roster[0]
+            attacker.makeAttack(roster,1)
+            timer = 0
+
+        timer += 1 #prevents multiple inputs
 
 
 class combatant():
@@ -50,18 +78,31 @@ class combatant():
         self.name = name
         self.enemOrFriend = enemOrFriend
         self.initiative = rollD20() + dexMod
+        self.hp = int(input("what is this fighters current HP? "))
+    def makeAttack(self, roster,targetIdx):
+        deadIdxs = []
+        print('\n')
+        dmg = int(input('How much damage does this attack deal? '))
+        for idx in range(targetIdx):
+            roster[idx].hp -= dmg
+            if roster[idx].hp <= 0:
+                print("%s has died!\n" % (roster[idx].name))
+                deadIdxs.append(idx)
+        return deadIdxs
+
+
 
 def main():
     combats = []
 
-    numCombats = input('How many combatants are there? ')
-    for idx in range(int(numCombats)):  # creates all combatants
-        idx = str(idx + 1)
-        name = 'combatant' + idx
-        name = combatant(name, 1, 2)
-        combats.append(name)
-
+    combats = createNPCfighters(combats)
+    combats = addPCs(combats)
     combats = sortByIntiative(combats)
+    print('\n')
+    print('\n')
 
     mainGameLoop(combats)
-main()
+
+
+if __name__ == '__main__':
+    main()
