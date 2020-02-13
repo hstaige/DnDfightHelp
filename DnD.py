b429eb51bd680608b595d3f0a929a7334faf9156
staigerh1 <staigerh1@gmail.com>
2020-02-12 19:39:00 -0500

    Commit for the day

--- DnD.py
+++ DnD.py
@@ -1,6 +1,7 @@
 import random
 import math
 import keyboard
+import time
 
 def rollD20():
     return random.randint(1, 20)
@@ -19,37 +20,64 @@ def sortByIntiative(arr):
     return arr
 
 
-def createNPCfighters(combats):
-    numCombats = int(input('How many enemys are there? '))
+def areYouTesting():
+    timer = 0
+    while timer < 100000:
+        if keyboard.is_pressed('s'):#standard
+            return True
+        timer += 1
+    else:
+        return False
+
+
+def createNPCfighters(combats,testing):
+
+    if testing:
+        numCombats = 9
+        hp = 9
+    else:
+        numCombats = int(input('How many enemys are there? ')) #npc count input
+
     for idx in range(numCombats):  # creates all npc combatants
         idx = str(idx + 1)
         name = 'Enemy' + ' ' + idx
-        name = combatant(name, 1, 2)
+        if not testing:
+            hp = int(input('What is the hp of enemy %d? ' % (idx+1)))
+        name = combatant(name, 1, 2, hp)
         combats.append(name)
 
     return combats
 
 
-def addPCs(combats):
-    numPlayers = int(input('How many PCs are there? '))
+def addPCs(combats,testing):
+    if testing:
+        numPlayers = 3
+        hp = 10
+        playerName = 'Testing!'
+    else:
+        numPlayers = int(input('How many PCs are there? '))
+
     for idx in range(numPlayers):
-        playerName = input('What is the name of PC %d? ' % (idx+1))
-        PC = combatant(playerName, 0, 2)
+        if not testing:
+            hp = int(input('What is the current hp of %s? ' % (playerName)))
+            playerName = input('What is the name of PC %d? ' % (idx+1))
+        PC = combatant(playerName, 0, 2, hp)
         combats.append(PC)
 
     return combats
 
 
-def printFighters(roster):
+def printFighters(roster): #need to add formatting, want to make it a table
+    print('%-5s%-12s' % ('Init|','Fighter Name|'))
     for fighter in roster:
-        print('%-2d %s' % (fighter.initiative, fighter.name))
-    print('All Done!\n')
+        print('%-5d%-12s' % (fighter.initiative, fighter.name))
+    print('\nAll Done!\n')
 
 
 def goDownList(roster):
     roster.append(roster[0])
     roster.pop(0)
-    return roster
+    return rosters
 
 
 def mainGameLoop(roster):
@@ -74,11 +102,11 @@ def mainGameLoop(roster):
 
 
 class combatant():
-    def __init__(self, name, enemOrFriend, dexMod):
+    def __init__(self, name, enemOrFriend, dexMod, currentHP):
         self.name = name
         self.enemOrFriend = enemOrFriend
         self.initiative = rollD20() + dexMod
-        self.hp = int(input("what is this fighters current HP? "))
+        self.hp = currentHP
     def makeAttack(self, roster,targetIdx):
         deadIdxs = []
         print('\n')
@@ -95,11 +123,11 @@ class combatant():
 def main():
     combats = []
 
-    combats = createNPCfighters(combats)
-    combats = addPCs(combats)
+    standardBool = areYouTesting()
+    combats = createNPCfighters(combats,standardBool)
+    combats = addPCs(combats,standardBool)
     combats = sortByIntiative(combats)
-    print('\n')
-    print('\n')
+    print('\n\n')
 
     mainGameLoop(combats)
 
